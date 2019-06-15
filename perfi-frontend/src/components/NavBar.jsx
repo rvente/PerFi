7bb3e3f3eb2bed43b6831a345941fe603488d740
Ralph Vente <venteralph@gmail.com>
2019-06-15 13:39:05 -0400

    add changes from master

--- perfi-frontend/src/components/NavBar.jsx
+++ perfi-frontend/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LoginComponent from './Login.jsx';
 
 
 var DumbComponent = () => {
@@ -14,21 +15,18 @@ function NavBar() {
       <div>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/Transaction/">Transaction</Link>
-            </li>
-            <li>
-              <Link to="/Budget/">Budget</Link>
-            </li>
+            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/Transaction/">Transaction</Link> </li>
+            <li> <Link to="/Budget/">Budget</Link> </li>
+            <li> <Link to="/Account/">Account</Link> </li>
           </ul>
         </nav>
         <Route exact path="/" exact component={DumbComponent} />
         <Route path="/Home/" component={DumbComponent} />
         <Route path="/Transaction/" component={DumbComponent} />
         <Route path="/Budget/" component={DumbComponent} />
+        <Route path="/Account/" component={LoginComponent}/>
+        <Route path="/NewAccount/" component={NewAccount}/>
       </div>
     </Router>
   );
