6b3f0621cef592da8cd729dd89f5883fc12b59cd
Ralph Vente <venteralph@gmail.com>
2019-06-17 15:23:05 -0400

    add new-transaction component

--- perfi-frontend/src/components/NavBar.jsx
+++ perfi-frontend/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import NewAccount from './NewAccount.jsx';
 import Login from './Login.jsx';
+import Transaction from './AddTransaction.jsx';
 
 const LoginComponent = () => (<Login />);
 
@@ -26,7 +27,7 @@ function NavBar() {
         </nav>
         <Route path="/" exact component={DumbComponent} />
         <Route path="/Home/" component={DumbComponent} />
-        <Route path="/Transaction/" component={DumbComponent} />
+        <Route path="/Transaction/" component={Transaction} />
         <Route path="/Budget/" component={DumbComponent} />
         <Route path="/Account/" component={LoginComponent}/>
         <Route path="/NewAccount/" component={NewAccount}/>
