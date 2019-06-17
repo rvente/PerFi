63e0448e3b9b74e5bcb40b70b2ea9bd2f910e471
Ralph Vente <venteralph@gmail.com>
2019-06-17 15:38:10 -0400

    move router to App.jsx from NavBar

--- perfi-frontend/src/components/App.jsx
+++ perfi-frontend/src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from './NavBar';
+import NewAccount from './NewAccount.jsx';
+import Login from './Login.jsx';
+import Transaction from './AddTransaction.jsx';
 import '../css/App.css';
 import '../css/Global.css';
+
+const LoginComponent = () => (<Login />);
+
+// dumb component used for templating
+var DumbComponent = () => {return(<div></div>);};
 
 function App() {
   return (
     <div className="App">
-        <NavBar/>
+      <Router>
+        <div>
+          <NavBar/>
+          <Route path="/" exact component={DumbComponent} />
+          <Route path="/Home/" component={DumbComponent} />
+          <Route path="/Transaction/" component={Transaction} />
+          <Route path="/Budget/" component={DumbComponent} />
+          <Route path="/Account/" component={LoginComponent}/>
+          <Route path="/NewAccount/" component={NewAccount}/>
+        </div>
+      </Router>
     </div>
   );
 }
--- perfi-frontend/src/components/NavBar.jsx
+++ perfi-frontend/src/components/NavBar.jsx
@@ -1,38 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import NewAccount from './NewAccount.jsx';
-import Login from './Login.jsx';
-import Transaction from './AddTransaction.jsx';
-
-const LoginComponent = () => (<Login />);
-
-// dumb component used for templating
-var DumbComponent = () => {
-    return(
-        <div></div>
-    );
-};
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/Transaction/">Transaction</Link> </li>
-            <li> <Link to="/Budget/">Budget</Link> </li>
-            <li> <Link to="/Account/">Account</Link> </li>
-          </ul>
-        </nav>
-        <Route path="/" exact component={DumbComponent} />
-        <Route path="/Home/" component={DumbComponent} />
-        <Route path="/Transaction/" component={Transaction} />
-        <Route path="/Budget/" component={DumbComponent} />
-        <Route path="/Account/" component={LoginComponent}/>
-        <Route path="/NewAccount/" component={NewAccount}/>
-      </div>
-    </Router>
+      <nav>
+        <ul>
+          <li> <Link to="/">Home</Link> </li>
+          <li> <Link to="/Transaction/">Transaction</Link> </li>
+          <li> <Link to="/Budget/">Budget</Link> </li>
+          <li> <Link to="/Account/">Account</Link> </li>
+        </ul>
+      </nav>
   );
 }
 
