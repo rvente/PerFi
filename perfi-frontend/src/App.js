06c9f017c3d9e6e599c20992e866c43070072f5d
MarjanGit <mansar16@huntersoe.org>
2019-06-18 14:52:59 -0400

    add mandatory package

---
+++ perfi-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+import '../css/App.css';
+import '../css/Global.css';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+    </div>
+  );
+}
+
+export default App;
