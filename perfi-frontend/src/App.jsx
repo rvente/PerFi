62ebb04bb4a75b88b5da3f3f4e4e54413d61790d
MarjanGit <mansar16@huntersoe.org>
2019-06-18 15:46:58 -0400

    moved backend to frontend

---
+++ perfi-frontend/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import NewAccount from "./components/NewAccount.jsx";
+import Login2 from "./components/Login.jsx";
+import Transaction from "./components/AddTransaction.jsx";
+import Login from "./components/login";
+import AccountPage from "./components/account-page";
+import HomePage from "./components/home-page";
+
+import "./css/App.css";
+import "./css/Global.css";
+
+const LoginComponent = () => <Login />;
+
+// dumb component used for templating
+var DumbComponent = () => {
+  return <div />;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/home" component={HomePage} />
+          <Route exact path="/account" component={AccountPage} />
+          <Route component={Login} />
+          <Route path="/" exact component={DumbComponent} />
+          <Route path="/Home/" component={DumbComponent} />
+          <Route path="/Transaction/" component={Transaction} />
+          <Route path="/Budget/" component={DumbComponent} />
+          <Route path="/Account/" component={LoginComponent} />
+          <Route path="/NewAccount/" component={NewAccount} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
--- perfi-frontend/src/index.js
+++ perfi-frontend/src/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+// import React from "react";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import store, { getMe } from "./store/index";
+import Login from "./components/login";
+import AccountPage from "./components/account-page";
+import HomePage from "./components/home-page";
+import NavBar from "./components/NavBar";
+import NewAccount from "./components/NewAccount.jsx";
+import LoginComponent from "./components/Login.jsx";
+import Transaction from "./components/AddTransaction.jsx";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main = withRouter(
+  class extends Component {
+    componentDidMount() {
+      store.dispatch(getMe()).then(() => {
+        this.props.history.push("/");
+      });
+    }
+
+    render() {
+      return (
+        <Switch>
+          <Route exact path="/home" component={HomePage} />
+          <Route exact path="/account" component={AccountPage} />
+          <Route component={Login} />
+          <Route path="/" exact component={Login} />
+          <Route path="/Home/" component={HomePage} />
+          <Route path="/Transaction/" component={Transaction} />
+          {/* <Route path="/Budget/" component={DumbComponent} /> */}
+          <Route path="/Account/" component={LoginComponent} />
+          <Route path="/NewAccount/" component={NewAccount} />
+        </Switch>
+      );
+    }
+  }
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Main />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
