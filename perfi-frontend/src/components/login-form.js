44d48ca15732e1284a351eb65040b98a1d22b0a9
MarjanGit <mansar16@huntersoe.org>
2019-06-19 11:39:45 -0400

    added navbar to pages, switched up routes a bit

--- perfi-frontend/src/components/login-form.js
+++ perfi-frontend/src/components/login-form.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const LoginForm = (props) => {
-  const {handleSubmit} = props
+const LoginForm = props => {
+  const { handleSubmit } = props;
   return (
     <form onSubmit={handleSubmit}>
-      <div className='flex column'>
-        <div className='flex column m1'>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' className='input' />
+      <div className="flex column">
+        <div className="flex column m1">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" className="input" />
         </div>
-        <div className='flex column m1'>
-          <label htmlFor='email'>Password</label>
-          <input type='password' name='password' className='input' />
+        <div className="flex column m1">
+          <label htmlFor="email">Password</label>
+          <input type="password" name="password" className="input" />
         </div>
-        <div className='m1'>
-          <button type='submit' className='btn bg-blue white p1 rounded'>Submit</button>
+        <div className="m1">
+          <button type="submit" className="btn bg-blue white p1 rounded">
+            Submit
+          </button>
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
