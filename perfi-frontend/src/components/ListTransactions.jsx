884c8d14a4ae8a53e40b0a1ad64ad94a2d321df9
MarjanGit <mansar16@huntersoe.org>
2019-06-20 11:37:01 -0400

    sort by date complete

--- perfi-frontend/src/components/ListTransactions.jsx
+++ perfi-frontend/src/components/ListTransactions.jsx
@@ -12,7 +12,19 @@ class ListTransactions extends Component {
   }
 
   render() {
-    let trans = this.props.transactions.map(trans => (
+    let sorted = this.props.transactions.sort((a, b) => {
+      let x = a.date.replace(/-/g, "").replace(/[\/]/g, "");
+      let y = b.date.replace(/-/g, "").replace(/[\/]/g, "");
+      if (x < y) {
+        return 1;
+      }
+      if (x > y) {
+        return -1;
+      }
+      return 0;
+    });
+
+    let trans = sorted.map(trans => (
       <div className="card card-portrait">
         <ul>
           <li>Date: {trans.date} </li>
