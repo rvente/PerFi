e537d66d62b8756e14d5b396a7f2ff170029aa89
Belinda Liu <belinda.liu@rocketmail.com>
2019-06-20 19:01:22 -0400

    added total spent for budget as well as user budget

--- perfi-frontend/src/components/Budget.jsx
+++ perfi-frontend/src/components/Budget.jsx
@@ -1,16 +1,51 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import { connect } from "react-redux";
 
 class Budget extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+  }
+  state = {
+    totalSpent:0 
+    };
+
+  componentDidMount()
+  {
+      let total = 0;
+      for (var i = 0; i < this.props.transactions.length; i++)
+      {
+        total = total + this.props.transactions[i].cost;
+      }
+      this.setState({
+        totalSpent: total
+      })
+  }
+
   render() {
     return (
       <div>
         <NavBar />
-        THIS IS THE BUDGET PAGE
+        BUDGET: {this.props.user.budget}
+        <br></br>
+        <div>
+        TOTAL SPENT: {this.state.totalSpent}
+        </div>
       </div>
     );
   }
 }
 
-export default Budget;
+// get transactions from react store
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    transactions: state.transactions
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(Budget);
